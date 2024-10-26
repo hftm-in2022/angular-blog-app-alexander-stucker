82f57722bc1a828e12561f0bfac84b3b611a2b51
Alexander Stucker <alexander.stucker@hftm.ch>
2024-10-26 20:29:01 +0200

    feat: changed blog.ts to zod

--- src/app/interface/blog.ts
+++ src/app/interface/blog.ts
@@ -1,22 +1,27 @@
-export interface Comment {
-  author: string;
-  content: string;
-  createdAt: string;
-  id: number;
-  updatedAt: string;
-}
+import { z } from 'zod';
 
-export interface Blog {
-  author: string;
-  comments: Comment[];
-  content?: string;
-  contentPreview: string;
-  createdAt: string;
-  createdByMe: boolean;
-  headerImageUrl: string;
-  id: number;
-  likedByMe: boolean;
-  likes: number;
-  title: string;
-  updatedAt: string;
-}
+export const commentSchema = z.object({
+  author: z.string(),
+  content: z.string(),
+  createdAt: z.string(),
+  id: z.number(),
+  updatedAt: z.string(),
+});
+
+export const blogSchema = z.object({
+  author: z.string(),
+  comments: z.array(commentSchema),
+  content: z.string().optional(),
+  contentPreview: z.string(),
+  createdAt: z.string(),
+  createdByMe: z.boolean(),
+  headerImageUrl: z.string(),
+  id: z.number(),
+  likedByMe: z.boolean(),
+  likes: z.number(),
+  title: z.string(),
+  updatedAt: z.string(),
+});
+
+export type Comment = z.infer<typeof commentSchema>;
+export type Blog = z.infer<typeof blogSchema>;
