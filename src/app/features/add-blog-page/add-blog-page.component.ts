3f12b78412ffcaa0df5487a1c2d8b5c559b104e4
Alexander Stucker <alexander.stucker@hftm.ch>
2024-12-23 23:02:37 +0100

    feat: added html and scss for add-blog-page with working back button

--- src/app/features/add-blog-page/add-blog-page.component.ts
+++ src/app/features/add-blog-page/add-blog-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-blog-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-blog-page.component.html',
   styleUrl: './add-blog-page.component.scss',
 })
